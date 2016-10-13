5e733e9faed2838f70d33acbb935ade7e0854b00
John Tompkins <john.tompkins@csgi.com>
2016-10-13 14:16:52 -0500

    added form validation to the login form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-
+$(".login-form").validate({
+	rules: {
+		login: {
+			required: true,
+			email: true,
+			minlength: 5
+		},
+		password: {
+			required: true,
+			minlength: 5
+		}
+	},
+	//For custom messages
+	messages: {
+		uname: {
+			required: "Enter a username",
+			minlength: "Enter at least 5 characters"
+		},
+		curl: "Enter your website",
+	},
+	errorElement: 'div',
+	errorPlacement: function (error, element) {
+		var placement = $(element).data('error');
+		if (placement) {
+			$(placement).append(error)
+		} else {
+			error.insertAfter(element);
+		}
+	}
+});
