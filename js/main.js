0a3cfbca47eee8f2fb8d69fdead37bbb583a590b
John Tompkins <john.tompkins@csgi.com>
2016-10-13 17:55:05 -0500

    added login page as a template. created functions to be called for each template that invokes the javascript needed for each page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+	navigate('login');
+});
+
+var currentTemplate = '',
+	templateContainer = $('#template');
+
+function navigate(template) {
+	currentTemplate = template;
+	templateContainer.effect('slide', {
+		direction: 'left',
+		mode: 'hide',
+		complete: function () {
+			templateContainer.empty().show();
+			var templateHtml = $('#' + template).html();
+			templateContainer.append(templateHtml);
+			templateContainer.find('[style="display: none;"]').effect('slide', {
+				direction: 'right',
+				mode: 'show'
+			}, 250);
+			$(document).unbind();
+			window[template]();
+		}
+	})
+};
+
+function login() {
 	$(".login-form").validate({
 		rules: {
 			login: {
@@ -19,7 +45,7 @@ $(document).ready(function () {
 			},
 			curl: "Enter your website",
 		},
-		submitHandler: function() {
+		submitHandler: function () {
 			navigate('selectType');
 		},
 		errorElement: 'div',
@@ -32,29 +58,16 @@ $(document).ready(function () {
 			}
 		}
 	});
-});
+};
 
-var currentTemplate = 'login',
-	templateContainer = $('#template');
-
-function appendHtml() {
-	var html = $('#hidden-template').html();
-	$('.template').append(html);
-}
-
-function navigate(template) {
-	currentTemplate = template;
-	templateContainer.effect('slide', {
-		direction: 'left',
-		mode: 'hide',
-		complete: function () {
-			templateContainer.empty().show();
-			var templateHtml = $('#' + template).html();
-			templateContainer.append(templateHtml);
-			templateContainer.find('[style="display: none;"]').effect('slide', {
-				direction: 'right',
-				mode: 'show'
-			}, 250);
-		}
-	}
-)};
+function selectType() {
+	$('.dropdown-button').dropdown({
+		inDuration: 300,
+		outDuration: 225,
+		constrain_width: false, // Does not change width of dropdown to that of the activator
+		hover: true, // Activate on hover
+		gutter: 0, // Spacing from edge
+		belowOrigin: false, // Displays dropdown below the button
+		alignment: 'left' // Displays dropdown with edge aligned to the left of button
+	});
+};
